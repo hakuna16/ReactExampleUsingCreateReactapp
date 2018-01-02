a1f8b5e0701261da1a8df368d83d7e7ef189aa4b
Rituj kumar <ritujkuamr@hotmail.com>
2018-01-02 13:41:36 +0530

    Login form

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,13 +8,13 @@ class Header extends Component {
             <Navbar>
               <Navbar.Header>
                 <Navbar.Brand>
-                  <a href="#">React-Bootstrap</a>
+                  <a href="">Your Application Name</a>
                 </Navbar.Brand>
               </Navbar.Header>
               <Nav>
-                  <NavItem eventKey={1} href="#">Link</NavItem>
-                    <NavItem eventKey={2} href="#">Link</NavItem>
-                      <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                  <NavItem eventKey={1} href="#">Home</NavItem>
+                    <NavItem eventKey={2} href="#">About</NavItem>
+                      <NavDropdown eventKey={3} title="Value for Dropdown" name="Value for Dropdown" id="basic-nav-dropdown">
                             <MenuItem eventKey={3.1}>Action</MenuItem>
                             <MenuItem eventKey={3.2}>Another action</MenuItem>
                             <MenuItem eventKey={3.3}>Something else here</MenuItem>
---
+++ src/components/Login.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import {Form,FormGroup,Col,FormControl, Checkbox, Button,ControlLabel} from 'react-bootstrap'
+
+const LoginForm = ({login, onClick, onChange, errors}) => {
+  return (<div className="container">
+    <Form>
+      <FormGroup controlId="formHorizontalEmail">
+        <Col componentClass={ControlLabel} sm={2}>
+          Email
+        </Col>
+        <Col sm={10}>
+          <FormControl type="email" placeholder="Email" />
+        </Col>
+      </FormGroup>
+
+      <FormGroup controlId="formHorizontalPassword">
+        <Col componentClass={ControlLabel} sm={2}>
+          Password
+        </Col>
+        <Col sm={10}>
+          <FormControl type="password" placeholder="Password"/>
+        </Col>
+      </FormGroup>
+
+      <FormGroup>
+        <Col smOffset={2} sm={10}>
+          <Checkbox>Remember me</Checkbox>
+        </Col>
+      </FormGroup>
+
+      <FormGroup>
+        <Col smOffset={2} sm={10}>
+          <Button type="submit" >
+            Sign in
+          </Button>
+        </Col>
+      </FormGroup>
+    </Form>
+  </div>);
+}
+
+export default LoginForm;
